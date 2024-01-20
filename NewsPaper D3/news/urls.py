02780935667baef9d0636c53998a85626ff606d6
DenisZ82 <148885923+DenisZ82@users.noreply.github.com>
2024-01-21 02:34:48 +1000

    Add files via upload

---
+++ NewsPaper D3/news/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import PostList, PostDetail, Search, NewsCreate, \
+    ArticleCreate, NewsEdit, ArticleEdit, NewsDelete, ArticleDelete, subscriptions
+
+urlpatterns = [
+    path('', PostList.as_view(), name='post_list'),
+    path('<int:pk>', PostDetail.as_view(), name='post_detail'),
+    path('search/', Search.as_view(), name='search'),
+    path('news/create/', NewsCreate.as_view(), name='news_create'),
+    path('article/create/', ArticleCreate.as_view(), name='article_create'),
+    path('news/<int:pk>/edit/', NewsEdit.as_view(), name='news_edit'),
+    path('article/<int:pk>/edit/', ArticleEdit.as_view(), name='article_edit'),
+    path('news/<int:pk>/delete/', NewsDelete.as_view(), name='news_delete'),
+    path('article/<int:pk>/delete/', ArticleDelete.as_view(), name='article_delete'),
+    path('subscriptions/', subscriptions, name='subscriptions')
+]
